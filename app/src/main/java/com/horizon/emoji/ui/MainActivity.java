876fdda399742d6f348f511ac8453db9a3f6664f
sonptt <sonptt@vng.com.vn>
2019-12-10 15:56:32 +0700

    Implement widget ZEmojiEditText

--- app/src/main/java/com/horizon/emoji/ui/MainActivity.java
+++ app/src/main/java/com/horizon/emoji/ui/MainActivity.java
@@ -1,12 +1,10 @@
 package com.horizon.emoji.ui;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.horizon.emoji.R;
-import com.horizon.emoji.emoji.ZEmojiParser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textEmoji = findViewById(R.id.textEmoji);
-
-        textEmoji.setText(ZEmojiParser.parse("hello :11: :01:", textEmoji.getPaint().getFontMetricsInt()));
     }
 }
