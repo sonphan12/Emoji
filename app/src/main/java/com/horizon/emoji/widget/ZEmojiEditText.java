84bd750e68fed4f89dfd624be0fedde818726450
phatlv2 <phatlv2@vng.com.vn>
2019-12-12 10:18:45 +0700

    update emoji editext

--- app/src/main/java/com/horizon/emoji/widget/ZEmojiEditText.java
+++ app/src/main/java/com/horizon/emoji/widget/ZEmojiEditText.java
@@ -1,8 +1,6 @@
 package com.horizon.emoji.widget;
 
 import android.content.Context;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.AttributeSet;
 
 import androidx.appcompat.widget.AppCompatEditText;
@@ -12,41 +10,19 @@ import com.horizon.emoji.emoji.ZEmojiParser;
 public class ZEmojiEditText extends AppCompatEditText {
     public ZEmojiEditText(Context context) {
         super(context);
-        init();
     }
 
     public ZEmojiEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
     }
 
     public ZEmojiEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
     }
 
-    private void init() {
-        addTextChangedListener(new EmojiTextWatcher(this));
-    }
-
-    private static class EmojiTextWatcher implements TextWatcher {
-
-        ZEmojiEditText mEmojiEditText;
-
-        EmojiTextWatcher(ZEmojiEditText emojiEditText) {
-            mEmojiEditText = emojiEditText;
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {}
-
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            ZEmojiParser.parse(s, mEmojiEditText.getPaint().getFontMetricsInt());
-        }
+    @Override
+    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        ZEmojiParser.parse(text, this.getPaint().getFontMetricsInt());
+        super.onTextChanged(text, start, lengthBefore, lengthAfter);
     }
 }
